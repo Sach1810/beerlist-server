47e081c50b0833081ab444d67536717e947e6f3a
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-05-01 17:31:12 +0300

    Lesson completed

--- public/views/RegisterView.js
+++ public/views/RegisterView.js
@@ -5,13 +5,25 @@ var RegisterView = Backbone.View.extend({
     'click .register': 'register'
   },
 
- register: function (e) {
+  register: function (e) {
     e.preventDefault();
 
     var user = new UserModel({
       username: this.$('#register-username').val(), 
       password: this.$('#register-password').val()
     });
+
+    user.save({}, { 
+      success: function (user) {
+        alert(user.get('username') +  ' is successfully registered');
+        beerRouter.navigate('/' , true);
+      },
+      error : function (user, response) {
+        console.log(user);
+        console.log(response);
+      }
+    });
+  },
 
     user.save({}, { 
       success: function (user) {
